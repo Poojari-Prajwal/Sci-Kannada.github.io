e5af2f4b6aa33081db2f5ba18f3ca04cc5b066c3
Poojari-Prajwal <84854216+Poojari-Prajwal@users.noreply.github.com>
2021-05-29 23:37:56 +0530

    Sci Kannada round 2

---
+++ sci.js
@@ -0,0 +1,107 @@
+            function f()
+            {
+                if(confirm("Are you sure want to proceed with this option?"))
+                {
+                document.getElementById("d2").style.display="none";
+                document.getElementById("d3").style.display="block";
+                }
+              
+             }
+             function btn()
+             {
+                 document.getElementById("d3").style.display="none";
+                document.getElementById("d2").style.display="block";
+
+             }
+             function f1()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('comp_lang_bg.jpg')";
+                 document.getElementById("para").innerHTML="COMPUTER LANGUAGE";
+             }
+            
+             function f2()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('tele_bg.jpg')";
+                 document.getElementById("para").innerHTML="TELECOMMUNICATION SYSTEM";
+             }
+             function f3()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('solar_bg.jpg')";
+                 document.getElementById("para").innerHTML="SOLAR ENERGY";
+             }
+             function f4()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('cyber_bg.jpg')";
+                 document.getElementById("para").innerHTML="CYBER SECURITY";
+             }
+             function f5()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('art_int_bg.jpg')";
+                 document.getElementById("para").innerHTML="ARTIFICIAL INTELLIGENCE";
+             }
+             function f6()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('sat_bg.jpg')";
+                 document.getElementById("para").innerHTML="SATELLITE";
+             }
+             function f7()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('e_waste_bg.jpg')";
+                 document.getElementById("para").innerHTML="E-WASTE MANAGEMENT";
+             }
+             function f8()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('opt_net_bg.jpg')";
+                 document.getElementById("para").innerHTML="OPTICAL NETWORK";
+             }
+             function f9()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('holo_bg.jpg')";
+                 document.getElementById("para").innerHTML="HOLOGRAPHY";
+             }
+             function f10()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('touch_bg.jpg')";
+                 document.getElementById("para").innerHTML="TOUCH SCREEN WORKING";
+             }
+             function f11()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('traffic_bg.jpg')";
+                 document.getElementById("para").innerHTML="TRAFFIC CONGESTION TRACKING";
+             }
+             function f12()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('search_bg.png')";
+                 document.getElementById("para").innerHTML="SEARCH ENGINE";
+             }
+             function f13()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('wifi_bg.jpg')";
+                 document.getElementById("para").innerHTML="WI-FI";
+             }
+             function f14()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('mom_bg.jpg')";
+                 document.getElementById("para").innerHTML="MARS ORBITER MISSION";
+             }
+             function f15()
+             {
+                 f();
+                 document.getElementById("d3").style.backgroundImage="url('3d_bg.jpg')";
+                 document.getElementById("para").innerHTML="3D PRINTER";
+             }
+            
